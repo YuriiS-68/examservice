777970aa1a3d43eed41ce91c92535e1542b63c5d
Yuvis <yurii_s@bigmir.net>
2022-02-01 12:18:34 +0200

    init commit

---
+++ src/main/java/pro/sky/examservice/ExamserviceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.examservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamserviceApplication.class, args);
+    }
+
+}
